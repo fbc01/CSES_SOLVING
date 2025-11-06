8a706f1a9d6e80496500e35ebc90f22a57fa07e3
Sourav Kumar <42874343+fbc01@users.noreply.github.com>
2025-11-06 19:33:15 +0530

    Add C++ template with basic structure

---
+++ cpp_template.cpp
@@ -0,0 +1,43 @@
+/*
+    g++ --std=c++17 a.cpp -o a.out
+    ./a.out
+*/
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<unordered_set>
+#include<stack>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<tuple>
+#include <fstream>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
